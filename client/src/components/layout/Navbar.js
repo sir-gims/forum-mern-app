75388c21410263e5162a65a2b2aee390acf9eb11
sir-gims <bakiuagim@gmail.com>
2020-04-25 14:17:48 +0200

    Authenticated navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,33 +11,30 @@ const Navbar = () => {
     logout();
   };
 
-    return (
+  const authLinks = (
     <Fragment>
-    <ul id="dropdown1" class="dropdown-content">
-      <li>
-        <Link to='/profile'>Profile</Link>
-      </li>   
+       <li>
+        <Link to='/'>Home</Link>
+      </li>
       <li>
         <Link to='/edit-data'>Edit Profile</Link>
       </li>
       <li>
         <Link to='/change-password'>Change password</Link>
       </li>
-    </ul>
-  <nav className="teal darken-3">
-    <div className="nav-wrapper">
-      <span className="brand-logo push-s2"></span>
-      <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-      <a href="#!" class="brand-logo">ζforum</a>
-    <ul class="right hide-on-med-and-down">
+      <li>
+        <Link to='/profile'>Profile</Link>
+      </li>   
       <li>
         <a onClick={onLogout} href="#!">
           Logout
         </a>
       </li>
-      <li>
-        <Link to='/'>Home</Link>
-      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
       <li>
         <Link to='/about'>About</Link>
       </li>
@@ -47,39 +44,24 @@ const Navbar = () => {
       <li>
         <Link to='/register'>Register</Link>
       </li>
+    </Fragment>
+  );
 
-      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Profile<i class="material-icons right">arrow_drop_down</i></a></li>
+    return (
+    <Fragment>
+  <nav className="teal darken-3">
+    <div className="nav-wrapper">
+      <span className="brand-logo push-s2"></span>
+      <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+      <a href="#!" class="brand-logo">ζforum</a>
+    <ul class="right hide-on-med-and-down">
+      {isAuthenticated ? authLinks : guestLinks}
     </ul>
     </div>
   </nav>
 
   <ul className="sidenav" id="mobile-demo">
-    <li>
-      <Link to='/'>Home</Link>
-    </li>
-    <li>
-      <Link to='/about'>About</Link>
-    </li>
-    <li>
-      <Link to='/profile'>Profile</Link>
-    </li>
-    <li>
-      <Link to='/login'>Login</Link>
-    </li>
-    <li>
-      <Link to='/register'>Register</Link>
-    </li>
-    <li>
-      <Link to='/edit-data'>Edit Profile</Link>
-    </li>
-    <li>
-      <Link to='/change-password'>Change password</Link>
-    </li>
-    <li>
-        <a onClick={onLogout} href="#!">
-          Logout
-        </a>
-      </li>
+    {isAuthenticated ? authLinks : guestLinks}
   </ul>
     </Fragment>
     )
