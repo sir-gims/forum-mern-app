284588cdeba8fb756e0be5eecf76cdfe9c06ae09
sir-gims <bakiuagim@gmail.com>
2020-04-25 15:22:48 +0200

    Created PrivateRoute Component and Implemented Private Routing

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import ChangeData from './components/auth/ChangeData';
 import ChangePassword from './components/auth/ChangePassword';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import AuthState from './context/auth/AuthState';
 import setAuthToken from './utils/setAuthToken';
@@ -43,15 +43,15 @@ function App() {
             <Switch>
               {/* <PrivateRoute exact path='/' component={Home} /> */}
               {/* <Route exact path='/' component={Home} /> */}
-              <Route exact path='/' component={Threads} />
-              <Route exact path='/profile' component={Profile} />
-              <Route exact path='/current' component={CurrentThread} />
-              <Route exact path='/newThread' component={newThread} />
-              <Route exact path='/comments' component={Comments} />
-              <Route exact path='/newComment' component={newComment} />
+              <PrivateRoute exact path='/' component={Threads} />
+              <PrivateRoute exact path='/profile' component={Profile} />
+              <PrivateRoute exact path='/current' component={CurrentThread} />
+              <PrivateRoute exact path='/newThread' component={newThread} />
+              <PrivateRoute exact path='/comments' component={Comments} />
+              <PrivateRoute exact path='/newComment' component={newComment} />
               <Route exact path='/about' component={About} />
-              <Route exact path='/edit-data' component={ChangeData} />
-              <Route exact path='/change-password' component={ChangePassword} />
+              <PrivateRoute exact path='/edit-data' component={ChangeData} />
+              <PrivateRoute exact path='/change-password' component={ChangePassword} />
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
             </Switch>
