62eebe87313f88efaa81d1ca29b4a3f9a0fa3ddf
sir-gims <bakiuagim@gmail.com>
2020-04-30 18:28:56 +0200

    added authContext inside ProfileCard component

--- client/src/components/users/ProfileCard.js
+++ client/src/components/users/ProfileCard.js
@@ -1,26 +1,33 @@
-import React, { Fragment } from 'react';
+import React, {Fragment, useState, useContext, useEffect} from 'react';
+import AuthContext from '../../context/auth/authContext';
 
 const ProfileCard = () => {
+    const authContext = useContext(AuthContext);
+
+    const {user, error, clearErrors, isAuthenticated} = authContext;
+
+    const ProfilePic = require(`../../../../public/img/users/${user.photo}`);
+
     return (
         <Fragment>
         <br/>
-        <div className="card medium">
-            <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src="images/office.jpg" />
+        <div className="card large row">
+            <div className="card-image waves-effect waves-block waves-light col m6 s6">
+            <img className="activator" src={ProfilePic} height="270" width="1600" />
             </div>
             <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
+            <span className="card-title activator grey-text text-darken-4">{user.name}<i className="material-icons right">more_vert</i></span>
             <div>
-            <p>Email: </p>
-            <p>NO Threads: </p>
-            <p>Role: </p>
+            <p>Email: {user.email} </p>
+            <p>About Me: {user.bio} </p>
+            <p>Role: {user.role} </p>
             </div>
             </div>
             <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-            <p>Email: </p>
-            <p>NO Threads: </p>
-            <p>Role: </p>           
+            <span className="card-title grey-text text-darken-4">{user.name}<i className="material-icons right">close</i></span>
+            <p>Email: {user.email} </p>
+            <p>About Me: {user.bio} </p>
+            <p>Role: {user.role} </p>           
             </div>
         </div>
         </Fragment>
--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
     req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
 
   await sharp(req.file.buffer)
-    .resize(500,500)
+    .resize(1400,1600)
     .toFormat('jpeg')
     .jpeg({quality: 90})
     .toFile(`public/img/users/${req.file.filename}`);
