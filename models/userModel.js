0ed48f6c05b88f26b813aba9cfdb7210b3fcb972
sir-gims <bakiuagim@gmail.com>
2020-04-30 13:30:02 +0200

    Changed Passwords Requirments

--- models/userModel.js
+++ models/userModel.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8,
+    minlength: 4,
     select: false
   },
   passwordConfirm: {
